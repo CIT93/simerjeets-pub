bd9442e3b7f404bc1eccf75acf24c85e10bd9116
Simerjeet Singh <simerjeetsingh50@gmail.com>
2024-08-29 22:04:22 -0700

    quotes, operators and primitives

---
+++ w3/main.js
@@ -0,0 +1,21 @@
+const myName = "Simer Singh";
+const myAge = 20;
+
+const jsIsWeird = myName + myAge;
+console.log(jsIsWeird);
+console.log(typeof jsIsWeird);
+
+// what is order of precedence?
+// It is essentially PEMDAS. 
+
+const results = 20 + 2 * 3;      // Multiplication first, then addition
+console.log(results);
+
+const amIHungry = false;
+console.log(typeof amIHungry);
+
+let myVar;
+console.log(typeof myVar);
+
+const myHeading = document.querySelector("h1");
+console.log(typeof myHeading);
